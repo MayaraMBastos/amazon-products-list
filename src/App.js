6f07c67186ae5fb4c6da37f725dd1f71414ac713
MayaraMB <maybastos2021@gmail.com>
2025-04-21 15:33:22 -0300

    feat: view list of products in home page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ProductsList from "./components/ProductsList";
 
 function App() {
-  const [keyword, setKeyword] = useState('');
   const [products, setProducts] = useState([]);
-  const [error, setError] = useState('');
+  const [keyword, setKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  // Função para buscar produtos
   const handleSearch = async () => {
-    if (!keyword) {
-      setError('Por favor, insira uma palavra-chave.');
-      return;
-    }
-
+    if (!keyword.trim()) return;
+    setLoading(true);
     try {
-      const response = await fetch(`http://localhost:3000/api/scrape?keyword=${encodeURIComponent(keyword)}`);
+      const response = await fetch(`http://localhost:3000/api/scrape?keyword=${keyword}`);
       const data = await response.json();
-      if (Array.isArray(data) && data.length > 0) {
-        setProducts(data);
-        setError('');
-      } else {
-        setError('Nenhum produto encontrado.');
-        console.log(data)
-        setProducts([]);
-      }
-    } catch (err) {
-      setError('Erro ao buscar os produtos.');
-      console.log(err);
+      setProducts(data.results);
+    } catch (error) {
+      console.error("Erro ao buscar produtos:", error);
       setProducts([]);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="App">
-      <h1>Search Products from Amazon</h1>
-      <input 
-        type="text" 
-        placeholder="Keyword..." 
+    <div>
+      <h1>Busca na Amazon</h1>
+      <input
+        type="text"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
+        placeholder="Digite o produto..."
       />
-      <button onClick={handleSearch}>Search</button>
-      {error && <p className="error">{error}</p>}
-      <div className="products">
-        {products.map((product, index) => (
-          <div className="product" key={index}>
-            <img src={product.imageUrl} alt={product.title} />
-            <h3>{product.title}</h3>
-            <p>Classificação: {product.rating}</p>
-            <p>Avaliações: {product.reviews}</p>
-          </div>
-        ))}
-      </div>
+      <button onClick={handleSearch}>Buscar</button>
+
+      {loading ? <p>Carregando...</p> : <ProductsList products={products} />}
     </div>
   );
 }
